f2f5d2b585360785bf5e008201fadca9fb458fcc
The-DarK-os <anisri477@gmail.com>
2023-06-11 14:13:34 +0530

    edited setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl2',
-      version='1.0.5',
+      version='1.0.6',
       description='Deep Reinforcement Learning for Tensorflow 2 Keras',
       author='Taylor McNally',
-      url='https://github.com/wau/keras-rl2',
+      url='https://github.com/The-DarK-os/keras-rl2',
       license='MIT',
       install_requires=['tensorflow'],
       extras_require={
